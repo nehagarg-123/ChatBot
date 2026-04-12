40d2722630400527a3dfa4928112a83ca466ff75
nehagarg-123 <nehagarg6201@gmail.com>
2026-04-12 17:50:07 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,6 +6,9 @@ const userSchema = new mongoose.Schema(
         name: { type: String, required: true, trim: true },
         email: { type: String, required: true, unique: true, lowercase: true, trim: true },
         password: { type: String, required: true, minlength: 6 },
+         memory: { type: String, default: '' },
+        pdfText: { type: String, default: '' },
+        
     },
     { timestamps: true }
 );
